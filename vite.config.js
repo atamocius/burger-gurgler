1489412f70bbf8865fe5bab78c5dfe175c764e97
Daniel Anton Mata <anton.mata@gmail.com>
2021-04-19 22:24:57 +0800

    Added deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/burger-gurgler/',
   plugins: [reactRefresh()],
   resolve: {
     alias: {
